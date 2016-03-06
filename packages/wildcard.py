4d49de3b9b71c866570623053eef330df6ed9eae
Anton A Shanaurin <grspectre@gmail.com>
2016-03-06 22:42:24 +0300

    Primitive realization of Wildcard class

---
+++ packages/wildcard.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+
+
+class Wildcard:
+    """Wildcard class"""
+
+    def __init__(self):
+        """Constructor"""
+        pass
+
+    def add_wildcard(self, wildcard):
+        pass
+
+    def set_wildcards(self, wildcards):
+        pass
+
+    def check(self, name):
+        pass
+
+    def __prepare_wildcard(self, wildcard):
+        pass
