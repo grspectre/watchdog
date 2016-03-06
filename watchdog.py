7ab0e5c71d8da25ab5f7a1733b788d61d0e5a9ee
Anton A Shanaurin <grspectre@gmail.com>
2016-03-06 13:24:09 +0300

    Neccesary configuration file

--- watchdog.py
+++ watchdog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+from multiprocessing import Queue, Process
 import os
+
+
+def init():
+    """init watchdog"""
+    pass
+
+if __name__=="__main__":
+    init()
