93ee216b5519245ed049cbb093fab04f20e439df
Anton A Shanaurin <grspectre@gmail.com>
2016-03-07 19:07:15 +0300

    Some Configuration class test

---
+++ tests/test_packages_configuration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 -*-
+from packages.configuration import Configuration
+import unittest
+import os
+
+
+class ConfigurationTest(unittest.TestCase):
+
+    def setUp(self):
+        current_path = os.path.dirname(__file__)
+        self.configuration_path = os.path.join(current_path, 'test_data', 'configuration')
+        self.test_config = Configuration(self.configuration_path, 'test')
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        with self.assertRaises(RuntimeError):
+            Configuration("/some_fantasy_path", 'name')
+        with self.assertRaises(RuntimeError):
+            Configuration(self.configuration_path, 'test_empty')
+
+if __name__=="__main__":
+    unittest.main()
