c0fdda6901c600fbbaa56b5ffeecb060d791aea9
Anton A Shanaurin <grspectre@gmail.com>
2016-03-07 19:07:54 +0300

    Wildcard class description

--- packages/wildcard.py
+++ packages/wildcard.py
@@ -3,19 +3,50 @@
 
 class Wildcard:
     """Wildcard class"""
+    __wildcards = []
 
     def __init__(self):
-        """Constructor"""
+        """
+        Constructor
+        """
         pass
 
-    def add_wildcard(self, wildcard):
+    def add(self, wildcard):
+        """
+        Add wildcard to array for checking
+        :type self: packages.wildcard.Wildcard
+        :type wildcard: str
+        :return: void
+        :raise RuntimeError
+        """
         pass
 
-    def set_wildcards(self, wildcards):
+    def set(self, wildcards):
+        """
+        Set wildcards to array for checking
+        :type self: packages.wildcard.Wildcard
+        :type wildcards: list
+        :return: void
+        :raise: RuntimeError
+        """
         pass
 
-    def check(self, name):
+    def clear(self):
+        """
+        Clear wildcards array
+        :type self: packages.wildcard.Wildcard
+        :return: void
+        """
+        self.__wildcards = []
+
+    def check(self, checking_string):
+        """
+        Check checking_string
+        :type self: packages.wildcard.Wildcard
+        :param checking_string:
+        :return:
+        """
         pass
 
-    def __prepare_wildcard(self, wildcard):
+    def __prepare(self, wildcard):
         pass
